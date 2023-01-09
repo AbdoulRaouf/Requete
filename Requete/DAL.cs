2c8abdcf24ec6a42f39784265a4b129825d28c94
Appi72020 <abdoulraouf72020@gmail.com>
2023-01-09 17:07:54 +0100

    Ajout de l'examen tp en commentaire dans DAL.cs

--- Requete/DAL.cs
+++ Requete/DAL.cs
@@ -114,3 +114,85 @@ public static class StringExtension
     public static void ToConsole(this object str) => Console.WriteLine(str);
     //"fd".ToConsole();
 }
+/*using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static OleDbCommand command;
+
+        public static string Ajouter()
+        {
+            string requête = "insert into Voiture values(?,?,?,?,?)";
+            return requête;
+        }
+        static void Main(string[] args)
+        {
+            string ChaineDeConnexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\abdou\\OneDrive\\Documents\\Vehicule.accdb";
+
+            OleDbConnection objetDeConnexion = new OleDbConnection(ChaineDeConnexion);
+            try
+            {
+                objetDeConnexion.Open();
+                Console.WriteLine("Connexion etablit avec sucsès");
+
+                string strAjout = Ajouter();
+
+                Console.WriteLine("Entrer l'immatriculation");
+
+                string Immatriculation = Console.ReadLine();
+
+                Console.WriteLine("Entrer Le Modele");
+
+                string Modele = Console.ReadLine();
+
+                Console.WriteLine("Entrer le Kilometrage");
+
+                int kilometrage = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entrer la couleur");
+
+                string couleur = Console.ReadLine();
+
+                Console.WriteLine("Entrer le prix");
+
+                float Prix = float.Parse(Console.ReadLine());
+
+                OleDbCommand command = new OleDbCommand(strAjout, objetDeConnexion);
+
+                command.Parameters.Add("Immatriculation", Immatriculation);
+
+                command.Parameters.Add("Modele", Modele);
+
+                command.Parameters.Add("kilometrage", kilometrage);
+
+                command.Parameters.Add("Couleur", couleur);
+
+                command.Parameters.Add("Prix", Prix);
+
+                command.ExecuteNonQuery();
+                Console.WriteLine("Operation reussit avec success");
+
+                Console.ReadLine();
+
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine("Erreur de connexion" + e.ToString());
+                Console.ReadLine();
+            }
+            finally
+            {
+                objetDeConnexion.Close();
+            }
+
+
+
+
+
+        }
+    }
+}*/
